222fb48cc319b42312dd55a96fa1f0e6c4db9825
raegan <jan.gajewski3@gmail.com>
2024-06-16 17:46:10 +0200

    List pool usage extended to other valiable places

--- VContainer/Assets/VContainer/Runtime/Internal/ListPool.cs
+++ VContainer/Assets/VContainer/Runtime/Internal/ListPool.cs
@@ -1,19 +1,19 @@
 using System;
 using System.Collections.Generic;
 
-namespace VContainer.Unity
+namespace VContainer.Internal
 {
-    static class UnityEngineObjectListBuffer<T> where T : UnityEngine.Object
+    internal static class ListPool<T>
     {
         const int DefaultCapacity = 32;
 
         [ThreadStatic] 
-        private static Stack<List<T>> _pool = new Stack<List<T>>(4);
+        private static readonly Stack<List<T>> _pool = new Stack<List<T>>(4);
         
         /// <summary>
         /// BufferScope supports releasing a buffer with using clause.
         /// </summary>
-        public struct BufferScope : IDisposable
+        internal readonly struct BufferScope : IDisposable
         {
             private readonly List<T> _buffer;
 
@@ -32,7 +32,7 @@ namespace VContainer.Unity
         /// Get a buffer from the pool.
         /// </summary>
         /// <returns></returns>
-        public static List<T> Get()
+        internal static List<T> Get()
         {
             if (_pool.Count == 0)
             {
@@ -47,7 +47,7 @@ namespace VContainer.Unity
         /// </summary>
         /// <param name="buffer"></param>
         /// <returns></returns>
-        public static BufferScope Get(out List<T> buffer)
+        internal static BufferScope Get(out List<T> buffer)
         {
             buffer = Get();
             return new BufferScope(buffer);
@@ -57,7 +57,7 @@ namespace VContainer.Unity
         /// Declare a buffer won't be used anymore and put it back to the pool.  
         /// </summary>
         /// <param name="buffer"></param>
-        public static void Release(List<T> buffer)
+        internal static void Release(List<T> buffer)
         {
             buffer.Clear();
             _pool.Push(buffer);
